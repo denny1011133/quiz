872a3aac95270220600e3c20bc82f11a15559952
denny <denny@playmore.com.hk>
2024-03-06 17:36:21 +0800

    feat: Login UI

--- app/layout.js
+++ app/layout.js
@@ -19,8 +19,8 @@ export default function RootLayout({ children }) {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>
+    <html lang="en" className="h-full bg-white">
+      <body className={`${inter.className} antialiased h-full`}>
         <div className="bg-white">
           <header className="absolute inset-x-0 top-0 z-50">
             <nav
@@ -28,7 +28,7 @@ export default function RootLayout({ children }) {
               aria-label="Global"
             >
               <div className="flex lg:flex-1">
-                <a href="#" className="-m-1.5 p-1.5">
+                <Link href="/" className="-m-1.5 p-1.5">
                   <span className="sr-only">Your Company</span>
                   <Image
                     src="/png/logo-no-background.png"
@@ -36,7 +36,7 @@ export default function RootLayout({ children }) {
                     height={64}
                     alt="logo"
                   />
-                </a>
+                </Link>
               </div>
               <div className="flex lg:hidden">
                 <button
@@ -50,24 +50,25 @@ export default function RootLayout({ children }) {
               </div>
               <div className="hidden lg:flex lg:gap-x-12">
                 {navigation.map((item) => (
-                  <a
+                  <Link
                     key={item.name}
                     href={item.href}
                     className="text-sm font-semibold leading-6 text-gray-900"
                   >
                     {item.name}
-                  </a>
+                  </Link>
                 ))}
               </div>
               <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-                <a
-                  href="#"
+                <Link
+                  href="/login"
                   className="text-sm font-semibold leading-6 text-gray-900"
                 >
                   Log in <span aria-hidden="true">&rarr;</span>
-                </a>
+                </Link>
               </div>
             </nav>
+
             <Dialog
               as="div"
               className="lg:hidden"
@@ -77,43 +78,37 @@ export default function RootLayout({ children }) {
               <div className="fixed inset-0 z-50" />
               <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                 <div className="flex items-center justify-between">
-                  <a href="#" className="-m-1.5 p-1.5">
-                    <span className="sr-only">Your Company</span>
-                    <img
-                      className="h-8 w-auto"
-                      src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                      alt=""
-                    />
-                  </a>
-                  <button
-                    type="button"
-                    className="-m-2.5 rounded-md p-2.5 text-gray-700"
-                    onClick={() => setMobileMenuOpen(false)}
-                  >
-                    <span className="sr-only">Close menu</span>
-                    <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-                  </button>
+                  <Link href="/" className="-m-1.5 p-1.5">
+                    <button
+                      type="button"
+                      className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                      onClick={() => setMobileMenuOpen(false)}
+                    >
+                      <span className="sr-only">Close menu</span>
+                      <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                    </button>
+                  </Link>
                 </div>
                 <div className="mt-6 flow-root">
                   <div className="-my-6 divide-y divide-gray-500/10">
                     <div className="space-y-2 py-6">
                       {navigation.map((item) => (
-                        <a
+                        <Link
                           key={item.name}
                           href={item.href}
                           className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                         >
                           {item.name}
-                        </a>
+                        </Link>
                       ))}
                     </div>
                     <div className="py-6">
-                      <a
-                        href="#"
+                      <Link
+                        href="/login"
                         className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                       >
                         Log in
-                      </a>
+                      </Link>
                     </div>
                   </div>
                 </div>
